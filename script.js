8d280f106f2d8b9cefe70b3fa0e893435534aa7d
Melissa Roig <melissa.roig@laposte.net>
2022-11-22 11:22:49 +0100

    addition, soustraction, multiplication, division

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('test')
+
+function add(a, b) {
+	return a + b;
+}
+let result = add(1, 1);
+alert(result);
+
+function getValue(){
+	var input = document.getElementById("sous").value;
+alert(input);
+}
+
+function soustrcation(a, b) {
+	return a - b;
+}
+result = soustrcation([-1], [-1])
+alert(result)
+
+function multiplication(a, b) {
+	return a * b;
+}
+result = multiplication (1, 1)
+alert(result)
+
+function division(a, b) {
+	return a / b;
+}
+result = division(1, 1)
+alert(result)
